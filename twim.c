679e550cdc3b39ec543eb229421b15d0fec9972c
cv007 <32439630+cv007@users.noreply.github.com>
2021-01-21 15:29:36 -0600

    Add files via upload

--- twim.c
+++ twim.c
@@ -15,6 +15,46 @@
     // private:
     //==========
 
+//pins
+
+/* twi0 master
+                mega0/DA    tiny0/1     tiny0/1 XX2 (8pin)
+default SCL      PA3          PB0         PA2
+default SDA      PA2          PB1         PA1
+    alt SCL      PC3          PA2         --
+    alt SDA      PC2          PA1         --
+*/
+
+#if defined(PORTMUX_TWISPIROUTEA) //mega0
+#define TWIM_PULL_DEFAULT() PORTA.PIN3CTRL |= 1<<3; PORTA.PIN2CTRL |= 1<<3
+#define TWIM_PULL_ALT()     PORTC.PIN3CTRL |= 1<<3; PORTC.PIN2CTRL |= 1<<3
+#define PORTMUX_DEFAULT()   PORTMUX.TWISPIROUTEA &= ~PORTMUX_TWI0_gm;
+#define PORTMUX_ALT()       (PORTMUX.TWISPIROUTEA & ~PORTMUX_TWI0_gm) | PORTMUX_TWI0_ALT2_gc;
+
+#elif defined(TWIROUTEA) //avrda
+#define TWIM_PULL_DEFAULT() PORTA.PIN3CTRL |= 1<<3; PORTA.PIN2CTRL |= 1<<3
+#define TWIM_PULL_ALT()     PORTC.PIN3CTRL |= 1<<3; PORTC.PIN2CTRL |= 1<<3
+#define PORTMUX_DEFAULT()   PORTMUX.TWIROUTEA &= ~PORTMUX_TWI0_gm;
+#define PORTMUX_ALT()       (PORTMUX.TWIROUTEA & ~PORTMUX_TWI0_gm) | PORTMUX_TWI0_ALT2_gc;
+
+#elif defined(PORTMUX_CTRLB) && defined(PORTMUX_TWI0_bm) //tiny0/1 w/alternate pins
+#define TWIM_PULL_DEFAULT() PORTB.PIN0CTRL |= 1<<3; PORTB.PIN1CTRL |= 1<<3
+#define TWIM_PULL_ALT()     PORTA.PIN2CTRL |= 1<<3; PORTA.PIN1CTRL |= 1<<3
+#define PORTMUX_DEFAULT()   PORTMUX.CTRLB &= ~PORTMUX_TWI0_bm;
+#define PORTMUX_ALT()       PORTMUX.CTRLB |= PORTMUX_TWI0_bm;
+
+
+#elif defined(PORTMUX_CTRLB) && !defined(PORTMUX_TWI0_bm) //tiny0/1 no alternate pins
+#define PORTMUX_DEFAULT()
+#define PORTMUX_ALT()
+#define TWIM_PULL_DEFAULT() PORTA.PIN2CTRL |= 1<<4; PORTA.PIN1CTRL |= 1<<4
+#define TWIM_PULL_ALT()     TWIM_PULL_DEFAULT()
+
+#else
+#error "Unknown portmux/pin settings for TWI0"
+#endif
+
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 
--- twis.c
+++ twis.c
@@ -12,6 +12,46 @@
     //  private:
     //============
 
+//pins
+
+/* twi0 master
+                mega0/DA    tiny0/1     tiny0/1 XX2 (8pin)
+default SCL      PA3          PB0         PA2
+default SDA      PA2          PB1         PA1
+    alt SCL      PC3          PA2         --
+    alt SDA      PC2          PA1         --
+*/
+
+#if defined(PORTMUX_TWISPIROUTEA) //mega0
+#define TWIS_PULL_DEFAULT()     PORTA.PIN3CTRL |= 1<<3; PORTA.PIN2CTRL |= 1<<3
+#define TWIS_PULL_ALT()         PORTC.PIN3CTRL |= 1<<3; PORTC.PIN2CTRL |= 1<<3
+#define TWIS_PORTMUX_DEFAULT()  PORTMUX.TWISPIROUTEA &= ~PORTMUX_TWI0_gm;
+#define TWIS_PORTMUX_ALT()      (PORTMUX.TWISPIROUTEA & ~PORTMUX_TWI0_gm) | PORTMUX_TWI0_ALT2_gc;
+
+#elif defined(TWIROUTEA) //avrda
+#define TWIS_PULL_DEFAULT()     PORTA.PIN3CTRL |= 1<<3; PORTA.PIN2CTRL |= 1<<3
+#define TWIS_PULL_ALT()         PORTC.PIN3CTRL |= 1<<3; PORTC.PIN2CTRL |= 1<<3
+#define TWIS_PORTMUX_DEFAULT()  PORTMUX.TWIROUTEA &= ~PORTMUX_TWI0_gm;
+#define TWIS_PORTMUX_ALT()      (PORTMUX.TWIROUTEA & ~PORTMUX_TWI0_gm) | PORTMUX_TWI0_ALT2_gc;
+
+#elif defined(PORTMUX_CTRLB) && defined(PORTMUX_TWI0_bm) //tiny0/1 w/alternate pins
+#define TWIS_PULL_DEFAULT()     PORTB.PIN0CTRL |= 1<<3; PORTB.PIN1CTRL |= 1<<3
+#define TWIS_PULL_ALT()         PORTA.PIN2CTRL |= 1<<3; PORTA.PIN1CTRL |= 1<<3
+#define TWIS_PORTMUX_DEFAULT()  PORTMUX.CTRLB &= ~PORTMUX_TWI0_bm;
+#define TWIS_PORTMUX_ALT()      PORTMUX.CTRLB |= PORTMUX_TWI0_bm;
+
+
+#elif defined(PORTMUX_CTRLB) && !defined(PORTMUX_TWI0_bm) //tiny0/1 no alternate pins
+#define TWIS_PORTMUX_DEFAULT()
+#define TWIS_PORTMUX_ALT()
+#define TWIS_PULL_DEFAULT() PORTA.PIN2CTRL |= 1<<4; PORTA.PIN1CTRL |= 1<<4
+#define TWIS_PULL_ALT()     TWIS_PULL_DEFAULT()
+
+#else
+#error "Unknown portmux/pin settings for TWI0"
+#endif
+
+
 static volatile u8      lastAddress_;
 static twis_callback_t  isrFuncCallback_;
 
