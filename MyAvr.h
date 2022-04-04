ef92341a5332dc9de8e8b79d3df3f77c7e237866
Guy Gastineau <strings.stringsandstrings@gmail.com>
2022-04-04 08:51:13 -0400

    Make setting F_CPU conditional.
    This lets users with different F_CPU settings use this library without
    needing to modify the library code if they set the macro in their GCC
    invocation with -DF_CPU=XXXXXUL.

--- MyAvr.h
+++ MyAvr.h
@@ -10,13 +10,11 @@
 //======================================================================
 //  global F_CPU for delays, or any timing calculations
 //======================================================================
+#ifndef F_CPU
 #define F_CPU 3333333ul
+#endif
 
 #include <util/delay.h>
 
 typedef uint8_t  u8;
 typedef uint16_t u16;
-
-
-
-
